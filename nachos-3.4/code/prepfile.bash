a8f58e0640510fe2e7215740e000e8ae5fb28af2
gogogy <ehsanpelatini@gmail.com>
2017-12-08 22:59:00 +0330

    Extract NachOS Files

---
+++ nachos-3.4/code/prepfile.bash
@@ -0,0 +1,7 @@
+export CSE120_HOST="-DHOST_i386"
+export CSE120_COMMON_CFLAGS="-m32"
+export CSE120_COMMON_ASFLAGS="--32"
+export CSE120_LDFLAGS="-m32"
+export CSE120_CFLAGS="-m32 -I./ -I../threads -g"
+export CSE120_GCC=/Users/ehsan/Documents/University/OS/Projects/Project2/Nachos_Operating_Systems_Course/mips/bin/
+export MAKE_CMD="make"
